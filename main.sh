5a84994fa1264410379d7f418c31552bf47ba0a8
qus0in <qus0in@gmail.com>
2024-01-08 16:20:05 +0900

    sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
 #!/bin/bash --login
-/usr/bin/python3 /home/ubuntu/upbit_propeht_ds/main.py >> /home/ubuntu/cron.log 2>&1
+/usr/bin/python3 /home/ubuntu/upbit_prophet_ds/main.py >> /home/ubuntu/cron.log 2>&1
